d5a0e7fadd8d4942f06d40d379a8ddf2dce85c7d
ZijunXiong <84898510+ZijunXiong@users.noreply.github.com>
2023-03-08 11:23:53 +0800

    Add files via upload

---
+++ Scripts/01_Genome_assembly/C.2b.SLR-superscaffolder.sh
@@ -0,0 +1,2 @@
+../software/SLR-superscaffolder/SLR/bin/FakeSOAPContig < input_genome.fasta 1> scaffold_1.soap.contig 2> scaffold_1.soap.ContigIndex
+../software/SLR-superscaffolder/SLR/scaffold/prepare.sh ./conf.ini
